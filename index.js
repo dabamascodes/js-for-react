2f5612810bf07e34a928fe8f8a2469cf8684945c
dabamascodes <daniel.6@live.com.mx>
2024-04-04 13:28:44 -0600

    Funciones de JS

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-console.log('Hola Mundo')
+
+function hello(){
+    /*
+    return 'Hola Mundo';
+    return 20;
+    return 20.3;
+    return true;
+    return [1,2,3];
+    return {name: 'daniel'}
+    */
+
+    // Functional Programming
+    // Funci'on que retorna otra funcion --> En React se usa mucho para los COMPONENTES.
+    return function () {
+        return 'hola mundo';
+    }
+
+}
+/*
+const result = hello();
+console.log(result);
+*/
+//console.log(hello());
+console.log(hello()());
+
+
+
+
+
