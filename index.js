6c6c9d0d76f6325fe97b61efafde9039f631b36f
dabamascodes <daniel.6@live.com.mx>
2024-04-04 13:41:45 -0600

    Parametros de funciones

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-
+/*
 function hello(){
-    /*
-    return 'Hola Mundo';
-    return 20;
-    return 20.3;
-    return true;
-    return [1,2,3];
-    return {name: 'daniel'}
-    */
+    
+    //return 'Hola Mundo';
+    //return 20;
+    //return 20.3;
+    //return true;
+    //return [1,2,3];
+    //return {name: 'daniel'}
+    
 
     // Functional Programming
     // Funci'on que retorna otra funcion --> En React se usa mucho para los COMPONENTES.
@@ -16,12 +16,42 @@ function hello(){
     }
 
 }
-/*
-const result = hello();
-console.log(result);
-*/
+
+//const result = hello();
+//console.log(result);
+
 //console.log(hello());
 console.log(hello()());
+*/
+
+/////////////////////////////////////////////////////
+/*
+function hello(name){
+    return 'Hola ' + name;
+}
+
+console.log(hello('Daniel'));
+console.log(hello('Jes'));
+console.log(hello('Eli'));
+*/
+
+function add (x, y) {
+    return x + y;
+}
+
+console.log(add(10, 20));
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
